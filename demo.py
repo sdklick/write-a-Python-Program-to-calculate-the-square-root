c29b876285057040fed22957544f6d479250f157
Sumanta Das <sumantad827@gmail.com>
2022-10-03 20:29:47 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,21 @@
+# write a Python Program to calculate the square root
+
+# first approach
+
+num=int(input("please input a number : "))
+
+myresult=num**0.5
+
+print(myresult)
+
+# second approach
+
+import math
+
+result=math.sqrt(num)
+
+print(result)
+
+
+
+
